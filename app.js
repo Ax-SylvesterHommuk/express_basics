20f60c2155ffa225aa30a161a730ceb31c4ff8a3
Ax-SylvesterHommuk <ax-sylvester.hommuk@khk.ee>
2023-01-25 13:36:10 +0200

    template loop use

--- app.js
+++ app.js
@@ -7,10 +7,19 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/user/:username', (req, res) => {
-    let user = req.params.username;
-    res.render('index', {username : user});
-});
+// app.get('/user/:username', (req, res) => {
+//     let user = req.params.username;
+//     res.render('index', {username : user});
+// });
+
+app.get('/questions', (req, res) => {
+    let questions = [
+        {title: "What is Node.js", user: "Kadi", votes: "10"},
+        {title: "What is Express.js", user: "Mike", votes: "8"},
+    ]
+
+    res.render('index', {questions:questions});
+})
 
 app.listen(3000, ()=> {
     console.log('Server started on http://localhost:3000 <--- Open link')
