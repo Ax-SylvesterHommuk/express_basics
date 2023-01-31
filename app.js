58772c8c9bdb93c47a9017ad84bd502e0d150ca9
Ax-SylvesterHommuk <ax-sylvester.hommuk@khk.ee>
2023-01-31 10:24:22 +0200

    post request use

--- app.js
+++ app.js
@@ -7,19 +7,13 @@ app.use(express.static('public'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// app.get('/user/:username', (req, res) => {
-//     let user = req.params.username;
-//     res.render('index', {username : user});
-// });
+app.get('/login', (req, res) => {
+    res.render('login')
+});
 
-app.get('/questions', (req, res) => {
-    let questions = [
-        {title: "What is Node.js", user: "Kadi", votes: "10"},
-        {title: "What is Express.js", user: "Mike", votes: "8"},
-    ]
-
-    res.render('index', {questions:questions});
-})
+app.get('/login', (req, res) => {
+    console.log('post request done')
+});
 
 app.listen(3000, ()=> {
     console.log('Server started on http://localhost:3000 <--- Open link')
